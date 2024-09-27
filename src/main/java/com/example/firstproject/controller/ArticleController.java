61d2d3131157169a81884808de0493eab8a7b793
ljh3 <ljh3@ez-charger.co.kr>
2024-09-27 14:36:44 +0900

    feat : 스프링부트3 자바 백엔드 개발 입문
    1. 3장 Create /  4장 Lombok

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,41 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        log.info(form.toString());
+//        System.out.println(form.toString());
+
+        //TODO 1. DTO를 Entity로 변환
+        Article article = form.toEntity();
+        log.info(article.toString());
+//        System.out.println(article.toString());
+
+        //TODO 2. Repository로 Entity를 DB에 저장
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
+
+        return "";
+    }
+}
