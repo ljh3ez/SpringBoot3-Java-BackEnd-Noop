91b80b010b8aff9fa18f2fd658cd5781233c8ee7
ljh3 <ljh3@ez-charger.co.kr>
2024-09-27 17:42:34 +0900

    feat : 스프링부트3 자바 백엔드 개발 입문
    1. 5장 Read / 6장 링크와 리다이렉트

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -36,6 +40,28 @@ public class ArticleController {
         log.info(saved.toString());
 //        System.out.println(saved.toString());
 
-        return "";
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show (@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        //TODO 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //TODO 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //TODO 3. 뷰 페이지 반환하기
+        return "articles/show";
+    };
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // TODO 1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        // TODO 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // TODO 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
