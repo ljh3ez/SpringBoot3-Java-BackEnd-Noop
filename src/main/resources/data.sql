527bbcb7da65b22884726a772ff864a2af259d7f
ljh3 <ljh3@ez-charger.co.kr>
2024-09-30 14:45:53 +0900

    feat : 스프링부트3 자바 백엔드 개발 입문
    1. 7장 update / 8장 delete

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO article(id, title, content) values(1, '가가가가', '1111');
+INSERT INTO article(id, title, content) values(2, '나나나나', '2222');
+INSERT INTO article(id, title, content) values(3, '다다다다', '3333');
+INSERT INTO article(id, title, content) values(4, '라라라라', '4444');
