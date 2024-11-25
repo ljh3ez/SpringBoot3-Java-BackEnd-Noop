108265153284e605e7726c4d1daf423eda9aedc7
ljh3 <ljh3@ez-charger.co.kr>
2024-11-25 11:09:09 +0900

    refactor : 유저정보 API 리팩토링

--- src/main/java/com/example/firstproject/httpdemo/controller/httpController.java
+++ src/main/java/com/example/firstproject/httpdemo/controller/httpController.java
@@ -4,6 +4,7 @@ import com.example.firstproject.httpdemo.service.httpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,10 @@ public class httpController {
     @Autowired
     private httpService httpService;
 
-    @GetMapping("/find/user")
+    @GetMapping("/find/user/{id}")
     @ResponseBody
-    public String getUserById () {
-        String response = httpService.getUsers("https://koreanjson.com/users/1");
+    public String getUserById(@PathVariable Long id) {
+        String response = httpService.getUsers("https://koreanjson.com/users/" + id);
         log.info("가져온 유저 데이터 : " + response);
         return response;
     }
